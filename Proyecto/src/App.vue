ca854b4bd5a8b60050d270b9aa3e04f954b09024
José Eduardo Henriques <zezeh35@hotmail.com>
2024-09-03 19:12:50 +0100

    playing around

--- Proyecto/src/App.vue
+++ Proyecto/src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import TestingApp from './components/TestingApp.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloMessage user="Zeze" />
     </div>
-  </header>
-
+  </header> -->
   <main>
-    <TheWelcome />
+    <TestingApp />
   </main>
 </template>
 
